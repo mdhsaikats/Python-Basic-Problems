2374a5bdb6d6d5805e24e6ad0843a716b15dc62b
mdhsaikats <saikatsiker2911@gmail.com>
2026-03-13 22:32:38 +0600

    changes

---
+++ multiplicationtable.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number: "))
+
+for n in range(1, 11):
+    result = num * n
+    print(num, "x", n, "=", result)
---
+++ sumofn.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number: "))
+total = 0
+
+for i in range(1, num + 1):
+    total = total + i
+
+print("Sum =", total)
